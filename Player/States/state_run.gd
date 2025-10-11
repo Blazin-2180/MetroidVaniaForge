c32c0e842e487036abb9dc14e263cda3e44b3a2b
Blazin-2180 <scotthoare89@gmail.com>
2025-10-11 21:01:01 +0100

    11/10 - 1
    Ep : 01-03 
    Run state added.
    Idle state added.

--- Player/States/state_run.gd
+++ Player/States/state_run.gd
@@ -6,6 +6,7 @@ func _init() -> void:
 
 # What happens when we enter the state ?
 func enter() -> void :
+	# Play animation
 	pass
 
 # What happens when we exit the state ?
@@ -14,12 +15,16 @@ func exit() -> void :
 
 # What happens when an input is pressed ?
 func handle_input( _event : InputEvent ) -> PlayerState : 
+	# Handle inputs besides movement
 	return next_state
 
 # What happens each process tick in this state ?
 func process( _delta : float ) -> PlayerState :
+	if player.direction.x == 0 :
+		return idle
 	return next_state
 
 # What happens each physics process tick in this state ?
 func physics_process( _delta : float ) -> PlayerState :
+	player.velocity.x = player.direction.x * player.move_speed
 	return next_state
