210107328a3b46d277f89baffc7b0cb6aa030739
Scott <scotthoare89@gmail.com>
2025-12-01 22:02:44 +0000

    01/12 - 1
    Ep : 02-08
    Light rays added.
    Updated the flicker code.

--- levels/01_dungeons/dungeon_torch_flicker.gd
+++ levels/01_dungeons/dungeon_torch_flicker.gd
@@ -1,13 +1,30 @@
-extends PointLight2D
+class_name LightFlicker extends PointLight2D
+
+#region
+
+## EXPORT VARIABLES ##
+@export var flicker_intensity : float = 0.1
+@export var flicker_frequency : float = 0.2
+
+## STANDARD VARIABLES ##
+var original_energy : float = 1.0
+
+#endregion
 
 
 func _ready() -> void:
+	original_energy = energy
 	flicker()
 	pass 
 
+
 func flicker() -> void :
-	energy = randf() * 0.3 + 2
-	scale = Vector2( 1, 1 ) * energy
-	await get_tree().create_timer( 0.133 ).timeout
+	var new_value : float = randf_range( -1, 1 ) * flicker_intensity
+	energy = original_energy + new_value
+	scale = Vector2 ( 1, 1 ) * original_energy 
+	await get_tree().create_timer( 
+		flicker_frequency + 
+		randf_range( flicker_frequency * -0.3, flicker_frequency * 0.3) 
+		).timeout
 	flicker()
 	pass
